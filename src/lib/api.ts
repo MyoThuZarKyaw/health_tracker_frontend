c7e65b9f4d63815405f5910c599f472c08bf6a6b
MyoThuZarKyaw <myothuzarkyaw.87@gmail.com>
2025-11-08 16:10:22 +1300

    change api to use the url from env

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8000';
+const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://health-tracker-backend-tau.vercel.app/';//'http://localhost:8000';
 
 // Create axios instance
 export const api = axios.create({
